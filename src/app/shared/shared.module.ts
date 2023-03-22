164357f1302bae6ee93b709fa893648aaba5bda0
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2023-03-22 05:27:32 +0100

    :sparkles: Form

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,12 @@
 import { CommonModule } from '@angular/common'
-import { NgModule } from '@angular/core'
+import { NgModule } from '@angular/core';
+import { FormComponent } from './form/form.component'
 
 @NgModule({
   imports: [CommonModule],
-  exports: [CommonModule],
+  exports: [CommonModule, FormComponent],
+  declarations: [
+    FormComponent
+  ],
 })
 export class SharedModule {}
